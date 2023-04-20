e238ff3458aa2a8748594782ccd873fd8cd4a07a
MART-DESKTOP\mart_ <tanathip.se@gmail.com>
2023-04-20 17:47:10 +0700

    Refactor to use configuration file instead

--- addons/http_logger_tool.py
+++ addons/http_logger_tool.py
@@ -1,6 +1,8 @@
 from mitmproxy import http
 import logging
 import json
+from configparser import ConfigParser
+import os
 
 
 class HTTPLoggerTools:
@@ -8,37 +10,49 @@ class HTTPLoggerTools:
     request_payload = []
     response_payload = []
     session_request_path = ""
+    url_path_for_log = ""
+    log_location = ""
 
     def __init__(self):
         logging.info("Starting logger tool")
         self.num = 0
+        self.read_configuration_file()
 
     def request(self, flow: http.HTTPFlow):
         self.session_request_path = flow.request.path
-        if ("/api/v1/" in self.session_request_path):
+        if (self.url_path_for_log in self.session_request_path):
             if (flow.request.method != "GET"):
                 logging.info("Found request pattern from configured URL: %s",
                              flow.request.url)
                 self.request_payload.append(json.loads(flow.request.content))
 
     def response(self, flow: http.HTTPFlow):
-        if ("/api/v1/" in self.session_request_path):
+        if (self.url_path_for_log in self.session_request_path):
             logging.info("Found response data from configured URL: %s",
                          flow.request.url)
             self.response_payload.append(json.loads(flow.response.content))
 
     def client_disconnected(self, data):
-        if ("/api/v1" in self.session_request_path):
+        if (self.url_path_for_log in self.session_request_path):
             self.request_payload.extend(self.response_payload)
             logging.info("Merged - %s", self.request_payload)
 
             self.num = self.num + 1
-            path = "c:\\temp\\filename_" + str(self.num) + ".json"
+            path = self.log_location + "\http_log_" + str(self.num) + ".json"
             with open(path, "w") as write_file:
                 json.dump(self.request_payload, write_file, indent=4)
 
             self.request_payload = []
             self.response_payload = []
+
+    def read_configuration_file(self):
+        thisfolder = os.path.dirname(os.path.abspath(__file__))
+        initfile = os.path.join(thisfolder, 'logger.cfg')
+        logging.info(thisfolder)
+        parser = ConfigParser()
+        parser.read(initfile)
+        self.url_path_for_log = parser.get('logger', 'url')
+        self.log_location = parser.get('logger', 'loglocation')
 
 
 addons = [HTTPLoggerTools()]
