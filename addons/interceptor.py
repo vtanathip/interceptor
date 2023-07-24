1b49f81421a955481fb7f4ea02b04c879fe3daa7
LABTOP-VTANATHIP <tanathip.se@gmail.com>
2023-07-24 19:53:12 +0700

    Simple mock response and addons

---
+++ addons/interceptor.py
@@ -0,0 +1,41 @@
+import os
+import json
+from mitmproxy import http
+
+
+class JsonLoader:
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def load_data(self):
+        try:
+            file_path = os.path.abspath(self.file_name)
+            with open(file_path, 'r') as file:
+                data = json.load(file)
+            return data
+        except FileNotFoundError:
+            raise FileNotFoundError(f"JSON file '{self.file_name}' not found.")
+        except json.JSONDecodeError:
+            raise ValueError(
+                f"Error decoding JSON in file '{self.file_name}'.")
+
+
+class MockingBird:
+
+    def response(self, flow: http.HTTPFlow):
+        print("Mock response")
+
+    def test(self):
+        loader = JsonLoader("config/mock.json")
+        try:
+            data = loader.load_data()
+            print("Name:", data["mock_services"][0])
+        except Exception as e:
+            print("Error:", e)
+
+
+addons = [MockingBird()]
+
+''' For test local run before using mitm '''
+# a = MockingBird()
+# a.test()
